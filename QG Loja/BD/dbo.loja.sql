142f0db2129a24658e11f7322e70d125c860fd6c
Daniel Siqueira <56129642+DFelps@users.noreply.github.com>
2023-05-15 21:49:36 -0300

    Add files via upload

---
+++ QG Loja/BD/dbo.loja.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Usuario] NCHAR(15) NOT NULL, 
+    [Email] NCHAR(30) NOT NULL, 
+    [Senha] NCHAR(15) NOT NULL, 
+    [Games] INT NULL
+)
